a0e9e96d6aa3c0ff08d004b43343dacdfe94c829
zaval <dzavalny@gmail.com>
2015-10-22 01:35:08 +0300

    fream macroses for sublime text

---
+++ fream_macroses.py
@@ -0,0 +1,133 @@
+import sublime, sublime_plugin
+import re
+from urllib.parse import unquote
+
+
+class Fream_macrosesCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sel = self.view.sel()
+
+		if not sel:
+			return
+
+		macro = self.view.substr(sel[0])
+		replace = self.process_request(macro)
+
+		if replace:
+
+			self.view.erase(edit, sel[0])
+			self.view.insert(edit, sel[0].a, replace)
+
+	def process_request(self, text):
+
+		if re.match(r'^\s*http\s\S+$', text):
+			res = re.search(r'^(\s*)http\s+(\S+)$', text)
+			if not res:
+				return
+
+			if res.group(2).startswith('http'):
+				result = res.group(1) + "page = self.http.get('" + res.group(2) + "')"
+			else:
+				result = res.group(1) + "page = self.http.get(" + res.group(2) + ")"
+
+			return result
+
+		elif re.match(r'^\s*http\s\S+\s+\S+$', text):
+			res = re.search(r'^(\s*)http\s+(\S+)\s+(\S+)$', text)
+			if not res:
+				return
+
+			ajax = ''
+			if res.group(3) == 'ajax':
+				ajax = '.ajax()'
+
+			if res.group(2).startswith('http'):
+				result = res.group(1) + "page = self" + ajax
+				+ ".http.get('" + res.group(2) + "')"
+			else:
+				result = res.group(1) + "page = self" + ajax
+				+ ".http.get(" + res.group(2) + ")"
+
+			return result
+
+		elif re.match(r'^\s*pd\s+.+$', text):
+			res = re.search(r'(\s*)pd\s+(.+)$', text)
+			if not res:
+				return
+
+			elems = [v for v in res.group(2).split('&') if v.strip()]
+			result = res.group(1) + 'data = {\n'
+
+			for elem in elems:
+
+				try:
+					key, val = elem.split('=')
+				except:
+					key = elem
+					val = ''
+
+				key = unquote(key)
+				val = unquote(val)
+				result += res.group(1) + "\t'{}': '{}',\n".format(key, val)
+
+			result += res.group(1) + '}'
+			return result
+
+		elif re.match(r'^f$', text):
+			return '.format()'
+
+		elif re.match(r'^b$', text):
+			return '<b>{}</b>'
+
+		elif re.match(r'^i$', text):
+			return '<i>{}</i>'
+
+		elif re.match(r'^\s*(?:parse|parse_all)\s\S+', text):
+			res = re.search(r'^(\s*)(parse|parse_all)\s+(\S+)$', text)
+			if not res:
+				return
+
+			result = '{0}{2} = hlp.{1}(r\'([^"]+)\', page)\n{0}if not {2}:\n{0}\tself.log("не смогли достать {2}", "!")\n{0}\treturn False\n{0}else:\n{0}\tself.log("достали {2}: %s" % {2}, "+")'.format(*list(res.groups()))
+			return result
+
+		elif re.match(r'^\s*l\s.+?\s*(?:\+|\*|\-|~|!|)', text):
+			res = re.search(r'^(\s*)l\s(.+?)\s*(\+|\*|\-|~|!*)$', text)
+			if not res:
+				return
+
+			args = [
+				res.group(1),
+				"'" if ' ' in res.group(2) else "",
+				res.group(2),
+				'' if not res.group(3) else ", '{}'".format(res.group(3))
+			]
+
+			result = "{0}self.log({1}{2}{1}{3})".format(*args)
+			return result
+
+		elif re.match(r'^\s*cnf\s+\S+\s*\S*', text):
+			res = re.search(r'^(\s*)cnf\s+(\S+)\s*(\S*)', text)
+			if not res:
+				return
+
+			result = "{0}{1} = self.cnf.get{2}('{1}')".format(*list(res.groups()))
+			return result
+
+		elif re.match(r'^\s*cnt\s+\S+', text):
+			res = re.search(r'^(\s*)cnt\s+(\S+)', text)
+			if not res:
+				return
+
+			result = "{0}self.hlp.cnt('{1}')".format(*list(res.groups()))
+			return result
+
+		elif re.match(r'^\s*slp\s+\S+', text):
+			res = re.search(r'^(\s*)slp\s+(\S+)', text)
+			if not res:
+				return
+
+			if re.sub(r'\d+', '', res.group(2)):
+				result = "{0}self.hlp.slp('{1}', log=self.log)".format(*list(res.groups()))
+			else:
+				result = "{0}self.hlp.slp({1}, log=self.log)".format(*list(res.groups()))
+			return result
